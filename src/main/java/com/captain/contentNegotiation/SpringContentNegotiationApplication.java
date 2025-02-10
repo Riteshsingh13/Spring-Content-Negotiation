b66b579676bcc59f26496fb3805a7bf50f0ed251
Ritesh <13singhritesh@gmail.com>
2025-02-10 21:01:46 +0530

    first step...

---
+++ src/main/java/com/captain/contentNegotiation/SpringContentNegotiationApplication.java
@@ -0,0 +1,13 @@
+package com.captain.contentNegotiation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContentNegotiationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContentNegotiationApplication.class, args);
+	}
+
+}
