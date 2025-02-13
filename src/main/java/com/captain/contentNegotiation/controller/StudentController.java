006d914b50c2ad0b4bd00a12caf9ce0f16d5d1f6
Ritesh <13singhritesh@gmail.com>
2025-02-13 15:48:05 +0530

    Created another method with produces type for that it will be detected by Accept HTTP Headers

--- src/main/java/com/captain/contentNegotiation/controller/StudentController.java
+++ src/main/java/com/captain/contentNegotiation/controller/StudentController.java
@@ -8,8 +8,15 @@ import com.captain.contentNegotiation.dto.Student;
 @RestController
 public class StudentController {
 
-	@GetMapping("/student")
+	@GetMapping(value = "/student", produces = "text/plain")
 	public String getStudentV1() {
+		System.out.println("plain text method executed.");
 		return new Student(1, "Axel", "axelb@inazuma.com").toString();
+	}
+	
+	@GetMapping(value = "/student", produces = "application/json")
+	public Student getStudentV2() {
+		System.out.println("json method executed.");
+		return new Student(1, "Axel", "axelb@inazuma.com");
 	}
 }
