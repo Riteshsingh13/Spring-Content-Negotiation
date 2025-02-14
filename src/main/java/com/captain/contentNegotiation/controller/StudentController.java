c073d1dae7bd84c6a5fe1313c3b7e5bc9f8ad62c
Ritesh <13singhritesh@gmail.com>
2025-02-14 23:00:08 +0530

    Added method to give response in pdf format

--- src/main/java/com/captain/contentNegotiation/controller/StudentController.java
+++ src/main/java/com/captain/contentNegotiation/controller/StudentController.java
@@ -1,5 +1,10 @@
 package com.captain.contentNegotiation.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,15 +13,24 @@ import com.captain.contentNegotiation.dto.Student;
 @RestController
 public class StudentController {
 
-	@GetMapping(value = "/student", produces = "text/plain")
+	@GetMapping(value = "/student", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String getStudentV1() {
 		System.out.println("plain text method executed.");
 		return new Student(1, "Axel", "axelb@inazuma.com").toString();
 	}
 	
-	@GetMapping(value = "/student", produces = "application/json")
+	@GetMapping(value = "/student", produces = {MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE})
 	public Student getStudentV2() {
 		System.out.println("json method executed.");
 		return new Student(1, "Axel", "axelb@inazuma.com");
+	}
+	
+	@GetMapping(value = "/student", produces = MediaType.APPLICATION_PDF_VALUE)
+	public InputStreamResource getStudentV3() throws FileNotFoundException {
+		System.out.println("pdf method executed.");
+		InputStreamResource ret = null;
+		FileInputStream fileInputStream = new FileInputStream("mystudent.pdf");
+		ret = new InputStreamResource(fileInputStream);
+		return ret;
 	}
 }
