fe9a3577c84300c99de702bdc43d8db452a190df
Ritesh <13singhritesh@gmail.com>
2025-02-17 11:07:42 +0530

    modified

--- src/main/java/com/captain/contentNegotiation/controller/StudentController.java
+++ src/main/java/com/captain/contentNegotiation/controller/StudentController.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.captain.contentNegotiation.dto.Student;
@@ -32,5 +34,10 @@ public class StudentController {
 		FileInputStream fileInputStream = new FileInputStream("mystudent.pdf");
 		ret = new InputStreamResource(fileInputStream);
 		return ret;
+	}
+	
+	@PostMapping(value = "/student", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE})
+	public void createStudent(@RequestBody Student student) {
+		System.out.println(student);
 	}
 }
